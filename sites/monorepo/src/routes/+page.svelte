a9c2ad9268d8a5e1610dbc44c3ecdd0bb2d3762d
kwangure <kaf.wangure@gmail.com>
2024-09-20 14:38:01 +0300

    feat: update content-thing

--- sites/monorepo/src/routes/+page.svelte
+++ sites/monorepo/src/routes/+page.svelte
@@ -1,1 +1,9 @@
-Hello <span class="text-blue-600">world!</span>
+<a href="/posts/post-filename-used-as-id">
+	This is a link to a Svelte route generated from Markdown.
+</a>
+
+<style>
+	a {
+		font-size: 120%;
+	}
+</style>
